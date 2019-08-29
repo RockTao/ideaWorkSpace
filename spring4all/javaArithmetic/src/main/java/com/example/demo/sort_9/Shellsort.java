3872e20f6e10af6d9c88ac566d886908a8a96af3
RockTao <tianshi2008tsj@163.com>
2019-08-29 16:27:54 +0800

    java数据结构和算法

--- spring4all/javaArithmetic/src/main/java/com/example/demo/sort_9/Shellsort.java
+++ spring4all/javaArithmetic/src/main/java/com/example/demo/sort_9/Shellsort.java
@@ -24,4 +24,34 @@ public class Shellsort {
     int[] arr= {1,11,9,8,7,2,3,6};
         System.out.println(Arrays.toString(sort(arr)));
     }
+
+    public static void shellKnuthSort(int[] array){
+        System.out.println("原数组为"+Arrays.toString(array));
+        int step = 1 ;
+        int len = array.length;
+        while(step <= len/3){
+            step = step*3 + 1;//1,4,13,40......
+        }
+        while(step > 0){
+            //分别对每个增量间隔进行排序
+            for(int i = step ; i < len ; i++){
+                int temp = array[i];
+                int j = i;
+                while(j > step-1 && temp <= array[j-step]){
+                    array[j] = array[j-step];
+                    j -= step;
+                }
+                array[j] = temp;
+            }//end for
+            System.out.println("间隔为"+step+"的排序结果为"+Arrays.toString(array));
+            step = (step-1)/3;
+        }//end while(step>0)
+
+        System.out.println("最终排序："+Arrays.toString(array));
+    }
+    @Test
+    public void testShellKnutSort(){
+        int[] array = {4,2,8,9,5,7,6,1,3,10};
+        shellKnuthSort(array);
+    }
 }
