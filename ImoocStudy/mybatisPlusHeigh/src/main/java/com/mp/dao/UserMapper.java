7fd8c07524df817527ab36b7285aa623df623961
Rock.Tao <tianshi2008tsj@163.com>
2021-04-22 23:29:36 +0800

    mysqlbatis-plus  高级自定义sql注入器

--- ImoocStudy/mybatisPlusHeigh/src/main/java/com/mp/dao/UserMapper.java
+++ ImoocStudy/mybatisPlusHeigh/src/main/java/com/mp/dao/UserMapper.java
@@ -1,5 +1,6 @@
 package com.mp.dao;
 
+import com.baomidou.mybatisplus.annotation.SqlParser;
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
@@ -12,8 +13,13 @@ import java.util.List;
 
 public interface UserMapper extends BaseMapper<User> {
 
-
+    @SqlParser(filter = true)//不增加租户信息   mp版本3.1之前的需要在全局配置中设置
     @Select("select * from user_heigh ${ew.customSqlSegment}")
     List<User> mySelectList(@Param(Constants.WRAPPER) Wrapper<User> wrapper);
 
+    /**
+     * 自定义sql注入器
+     * @return
+     */
+    int deleteAll();
 }
